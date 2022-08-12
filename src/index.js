3a16b3f098cb3982f98b118debdead3d7d605cb5
BlessedofGod <elman.yarov@yandex.ru>
2022-08-12 11:52:37 +0300

    add index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let res = str;
+  for (let i = 0; i < str.length / 2; i++) {
+    bracketsConfig.forEach(item => {
+      res = res.replace(item.join(''), '')
+    })
+  }
+  return res.length === 0 ? true : false
 }
